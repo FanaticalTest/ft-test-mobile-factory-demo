34f5a448a0969e422b3caa87f6f5125f41086b2b
Ziridis <samy@kacem.net>
2017-12-05 11:34:36 +0100

    Migrate from v1

--- src/test/java/com/fanaticaltest/fttestmobilefactorydemo/cucumber/CalculateSumSteps.java
+++ src/test/java/com/fanaticaltest/fttestmobilefactorydemo/cucumber/CalculateSumSteps.java
@@ -1,58 +1,73 @@
 package com.fanaticaltest.fttestmobilefactorydemo.cucumber;
 
-import cucumber.api.PendingException;
+import com.fanaticaltest.fttestmobilefactorydemo.devices.IosSimulator;
+import com.fanaticaltest.fttestmobilefactorydemo.features.CalculateSum;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.Before;
+import cucumber.api.java.After;
+
+import java.net.MalformedURLException;
 
 
-public class CalculateSumSteps {
+public class CalculateSumSteps extends CalculateSum{
+
+    @Before
+    public void before_scenario()
+    {
+        IosSimulator iosSimulator = new IosSimulator();
+        try {
+            this.driver = iosSimulator.beforeScenario();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void after_scenario()
+    {
+        IosSimulator iosSimulator = new IosSimulator();
+        iosSimulator.afterScenario(this.driver);
+    }
 
     @When("^a user is typing the first value \"([^\"]*)\"$")
-    public void a_user_is_typing_the_first_value(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void a_user_is_typing_the_first_value(String firstVal) throws Exception {
+        EnterFirstValue(firstVal);
     }
 
     @When("^a user is typing the second value \"([^\"]*)\"$")
-    public void a_user_is_typing_the_second_value(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void a_user_is_typing_the_second_value(String secondValue) throws Exception {
+        EnterSecondValue(secondValue);
     }
 
     @Then("^a user should see the sum equal to \"([^\"]*)\"$")
-    public void a_user_should_see_the_sum_equal_to(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void a_user_should_see_the_sum_equal_to(String sumResult) throws Exception {
+        CheckSumValue(sumResult);
     }
 
     @When("^a user is tapping Show alert$")
     public void a_user_is_tapping_Show_alert() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        TapLinkShowAlert();
     }
 
     @When("^a user is tapping on OK$")
     public void a_user_is_tapping_on_OK() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        TapOkShowAlert();
     }
 
     @Then("^a user should see in the result sum \"([^\"]*)\"$")
-    public void a_user_should_see_in_the_result_sum(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void a_user_should_see_in_the_result_sum(String sumResult) throws Exception {
+        CheckSumValue(sumResult);
     }
 
     @When("^a user move the slider to the value \"([^\"]*)\"$")
-    public void a_user_move_the_slider_to_the_value(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void a_user_move_the_slider_to_the_value(String val) throws Exception {
+        MoveSliderTo(val);
     }
 
     @Then("^the value returned is \"([^\"]*)\"$")
-    public void the_value_returned_is(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void the_value_returned_is(String val) throws Exception {
+        CheckSliderValueWithScreenShot(val);
     }
 
 }
--- src/test/java/com/fanaticaltest/fttestmobilefactorydemo/features/CalculateSum.java
+++ src/test/java/com/fanaticaltest/fttestmobilefactorydemo/features/CalculateSum.java
@@ -1,7 +1,75 @@
 package com.fanaticaltest.fttestmobilefactorydemo.features;
 
-/**
- * Created by samykacem on 04.12.17.
- */
+
+import com.fanaticaltest.ftappium.MobUI;
+import com.fanaticaltest.ftconfig.Property;
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CalculateSum {
+
+    private Property p = new Property("./src/main/resources/application.properties");
+    private String screenshotPath = p.read("global.screenshotPath");
+    private final Logger logger = LoggerFactory.getLogger("APPIUM-ROBOT");
+
+    private int tapDurationMillisecond = 1000;
+    private static final By FIRST_FIELD = By.name("IntegerA");
+    private static final By SECOND_FIELD = By.name("IntegerB");
+    private static final By COMPUTE_BUTTON = By.name("ComputeSumButton");
+    private static final By ANSWER_FIELD = By.name("Answer");
+    private static final By LINK_SHOW_ALERT = By.name("show alert");
+    private static final By ALERT_OK_BUTTON = By.name("OK");
+    private static final By SLIDER_APPELEM = By.xpath("//XCUIElementTypeSlider[@name=\"AppElem\"]");
+
+
+    public IOSDriver driver;
+
+    public void EnterFirstValue(String val)
+    {
+        MobUI mobUI = new MobUI(this.driver);
+        logger.info(mobUI.fillFieldBy(val,FIRST_FIELD));
+    }
+
+    public void EnterSecondValue(String val)
+    {
+        MobUI mobUI = new MobUI(this.driver);
+        logger.info(mobUI.fillFieldBy(val,SECOND_FIELD));
+        logger.info(mobUI.tapButtonBy(COMPUTE_BUTTON,1,tapDurationMillisecond));
+    }
+
+    public void CheckSumValue(String val)
+    {
+        MobUI mobUI = new MobUI(this.driver);
+        logger.info(mobUI.assertTextInElementBy(val, ANSWER_FIELD));
+    }
+
+    public void TapLinkShowAlert()
+    {
+        MobUI mobUI = new MobUI(this.driver);
+        logger.info(mobUI.tapButtonBy(LINK_SHOW_ALERT,1,tapDurationMillisecond));
+    }
+
+    public void TapOkShowAlert()
+    {
+        MobUI mobUI = new MobUI(this.driver);
+        logger.info(mobUI.tapButtonBy(ALERT_OK_BUTTON,1,tapDurationMillisecond));
+    }
+
+    public void MoveSliderTo(String val)
+    {
+        MobUI mobUI = new MobUI(this.driver);
+        logger.info(mobUI.swipeSliderBy(SLIDER_APPELEM,val));
+    }
+
+    public  void CheckSliderValueWithScreenShot(String val)
+    {
+        try {
+            MobUI mobUI = new MobUI(this.driver);
+            logger.info(mobUI.getScreenshot(screenshotPath, "slider"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
